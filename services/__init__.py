f5d9feac4f14a8cfbbe2c452b8e38519c5cdc2ad
okanberhoglu <okan.berhoglu@metu.edu.tr>
2026-01-08 19:12:27 +0300

    model_service is added to services __init__

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,5 @@
 from services.chat_service import ChatService
 from services.image_service import ImageService
+from services.model_service import ModelService
 
-__all__ = ['ChatService', 'ImageService']
+__all__ = ['ChatService', 'ImageService', 'ModelService']
